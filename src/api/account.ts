22d830e963134a25e1db2a53a01000d1d9d9e0e4
梁楷文 <lkw199711@163.com>
2024-08-06 11:24:01 +0800

    fix: 扁平化参数.

--- src/api/account.ts
+++ src/api/account.ts
@@ -2,14 +2,14 @@
  * @Author: lkw199711 lkw199711@163.com
  * @Date: 2023-03-17 20:18:30
  * @LastEditors: 梁楷文 lkw199711@163.com
- * @LastEditTime: 2024-05-17 17:17:20
+ * @LastEditTime: 2024-08-06 11:18:48
  * @FilePath: \smanga\src\api\account.ts
  */
 import {ajax} from './index';
 
 const userApi = {
 	async register(data: any) {
-		const res = ajax.post('user', {data});
+		const res = ajax.post('user', data);
 
 		return (await res).data;
 	},
@@ -37,7 +37,7 @@ const userApi = {
 	 * @return {*}
 	 */
 	async update_account(targetUserId: number, data: any) {
-		const res = ajax.patch(`user/${targetUserId}`, {data});
+		const res = ajax.put(`user/${targetUserId}`, data);
 
 		return (await res).data;
 	},
